87407cd8bdee8163059d80078b0cd279136695bb
ehsan <1374ea@gmail.com>
2025-04-22 19:02:58 +0330

    ♻️  Add 'build' to blacklisted directories in dir_tools.py

--- fixmate/helpers/dir_tools.py
+++ fixmate/helpers/dir_tools.py
@@ -3,5 +3,5 @@ def is_hidden_dir(dir_name: str) -> bool:
 
 
 def is_blacklisted_dir(dir_name: str) -> bool:
-    black_list = ["__pycache__", "venv", "env"]
+    black_list = ["__pycache__", "venv", "env", "build"]
     return dir_name in black_list
